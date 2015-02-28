e6554bec6580c04650f36517cac145068599defb
Andrew Grimm <andrew.j.grimm@gmail.com>
2015-02-28 23:25:15 +1100

    Add pending spec indicating some classes relating to Facing have incorrect values

---
+++ spec/integration_spec.rb
@@ -0,0 +1,49 @@
+require "factory_girl"
+require_relative "./factories/robot_factory"
+require "command_parser"
+
+RSpec.describe "integration specs" do
+  it "gives the correct result for example a" do
+    robot = FactoryGirl.build(:robot)
+    input = "PLACE 0,0,NORTH\n" \
+            "MOVE\n" \
+            "REPORT\n"
+
+    command_parser = CommandParser.new
+    commands = command_parser.parse(input)
+    commands.each { |command| command.run(robot) }
+
+    expect(robot.output).to eq("0,1,NORTH")
+  end
+
+  it "gives the correct result for example b" do
+    robot = FactoryGirl.build(:robot)
+    input = "PLACE 0,0,NORTH\n" \
+            "LEFT\n" \
+            "REPORT\n"
+
+    command_parser = CommandParser.new
+    commands = command_parser.parse(input)
+    commands.each { |command| command.run(robot) }
+
+    expect(robot.output).to eq("0,0,WEST")
+  end
+
+  it "gives the correct result for example c" do
+    robot = FactoryGirl.build(:robot)
+    input = "PLACE 1,2,EAST\n" \
+            "MOVE\n" \
+            "MOVE\n" \
+            "LEFT\n" \
+            "MOVE\n" \
+            "REPORT"
+
+    command_parser = CommandParser.new
+    commands = command_parser.parse(input)
+    commands.each { |command| command.run(robot) }
+
+    pending "Some classes related to Facing have incorrect values"
+
+    expect(robot.output).to eq("3,3,NORTH")
+  end
+end
