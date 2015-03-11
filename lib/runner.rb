97936517a1aa26b044cdec3eea925f522cff182f
Andrew Grimm <andrew.j.grimm@gmail.com>
2015-03-11 16:05:06 +1100

    Refactor

--- lib/runner.rb
+++ lib/runner.rb
@@ -14,12 +14,27 @@ class Runner
     input_parser = InputParser.new
     paddock, commands = input_parser.parse(input)
 
-    robot = Robot.new(reporter, paddock)
+    runner = new(reporter, paddock, commands, output_filename)
+    runner.run
+  end
 
-    commands.each do |command|
+  def initialize(reporter, paddock, commands, output_filename)
+    @reporter = reporter
+    @paddock = paddock
+    @commands = commands
+    @output_filename = output_filename
+  end
+
+  def run
+    run_commands
+    File.write(@output_filename, @reporter.output)
+  end
+
+  def run_commands
+    robot = Robot.new(@reporter, @paddock)
+
+    @commands.each do |command|
       command.run(robot)
     end
-
-    File.write(output_filename, reporter.output)
   end
 end
